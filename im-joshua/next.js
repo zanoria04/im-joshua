9d2bbbbb687a58b5f50ab7d6d63c24af6383b9b2
shufoo <40522799+zanoria04@users.noreply.github.com>
2018-08-30 12:57:44 +0800

    improving and debugging my site

--- im-joshua/next.js
+++ im-joshua/next.js
@@ -1,12 +1,3 @@
- function myMap() {
-var myCenter = new google.maps.LatLng(41.878114, -87.629798);
-var mapProp = {center:myCenter, zoom:12, scrollwheel:false, draggable:false, mapTypeId:google.maps.MapTypeId.ROADMAP};
-var map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
-var marker = new google.maps.Marker({position:myCenter});
-marker.setMap(map);
-}
-
-
 $(document).ready(function(){
     // Add smooth scrolling to all links in navbar + footer link
     $(".navbar a, footer a[href='#myPage']").on('click', function(event) {
